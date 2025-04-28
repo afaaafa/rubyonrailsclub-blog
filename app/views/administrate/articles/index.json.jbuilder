a21a4aa286c102a9a5247cea8ee1788c7263d02e
AF <af.arthurfaria@gmail.com>
2025-04-28 18:31:19 -0300

    separando a area publica e administrativa do site

---
+++ app/views/administrate/articles/index.json.jbuilder
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+json.array! @article, partial: 'articles/article', as: :article
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
+
+  namespace :administrate do
+    resources :articles
+  end
+
+  get "/inicio", to: "welcome#index"
+
 end
